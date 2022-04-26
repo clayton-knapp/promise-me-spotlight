6bf6b4af50056fb9aac5678edc86bf9299bf9874
Clayton Knapp <csjknapp@gmail.com>
2022-04-26 14:07:40 -0500

    followed along in class and added async and then functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import {
+  asyncGetQuotes,
+  thenGetQuotes,
+  asyncTryGetQuotes,
+  thenTryGetQuotes,
+  asyncFinallyGetQuotes,
+  thenFinallyGetQuotes,
+  thenAsyncGetQuotes,
+  asyncThenGetQuotes,
+} from './services/promise-me';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  //handle click is a higher order function - takes a function as an argument - a callback - in this case async get quotes
+  const handleClick = (callback) => {
+    const result = callback();
+    console.log(result);
+  };
+
+  return (
+    <>
+      <button onClick={asyncGetQuotes}>async get quotes</button>
+      <button onClick={thenGetQuotes}>then get quotes</button>
+      <button onClick={asyncTryGetQuotes}>async try get quotes</button>
+      <button onClick={thenTryGetQuotes}>then try get quotes</button>
+      <button onClick={asyncFinallyGetQuotes}>async finally get quotes</button>
+      <button onClick={thenFinallyGetQuotes}>then finally get quotes</button>
+      <button onClick={thenAsyncGetQuotes}>then async get quotes</button>
+      <button onClick={asyncThenGetQuotes}>async then get quotes</button>
+    </>
+  );
 }
